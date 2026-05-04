9290f43a8cd7a4783279ddbcf82c44ecac9804d3
umut92115-spec <umut92115@gmail.com>
2026-05-04 12:55:56 +0300

    Fix: Allow Googlebot to access _next and static assets (fixes broken rendering in Search Console)

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/api/', '/_next/', '/static/'],
+      disallow: ['/api/'],
     },
     sitemap: 'https://kpsscografya.com.tr/sitemap.xml',
   }
